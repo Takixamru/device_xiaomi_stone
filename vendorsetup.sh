53c14980ce8f6919c6cade74d7c6a28757d1f031
freakyfrosty <bhabanidas431@gmail.com>
2024-04-18 21:11:49 +0530

    stone: Track Everything From My Own Repos
    * Org Dropped Support For Android 13

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-20 device/xiaomi/sm6375-common
+git clone --depth=1 https://github.com/Takixamru/device_xiaomi_sm6375-common.git -b lineage-20 device/xiaomi/sm6375-common
 
-git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git vendor/xiaomi
+git clone --depth=1 https://github.com/Takixamru/vendor_xiaomi.git vendor/xiaomi
 
-git clone --depth=1 https://github.com/derp-stone/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/Takixamru/Kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
 
-git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Takixamru/device_xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
 
